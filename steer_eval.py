e30b4c9f5b5c324ea4bea4416401d574577d94b2
AMLeng <aleng@princeton.edu>
2017-07-24 12:50:41 -0700

    Adding queue runners to eval

--- steer_eval.py
+++ steer_eval.py
@@ -37,95 +37,80 @@ import sys
 
 import numpy as np
 import tensorflow as tf
-
 import steer
 import steer_input
 
-FLAGS = tf.app.flags.FLAGS
 EVAL_DIR='tmp/steering_eval'
 CHECKPOINT_DIR='tmp/steering_train'
 NUM_EXAMPLES=steer_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
-
-
-def eval_once(saver, loss_op):
-  """Run Eval once.
-  Args:
-    saver: Saver.
-    summary_writer: Summary writer.
-    summary_op: Summary op.
-  """
-  with tf.Session() as sess:
-    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
-    if ckpt and ckpt.model_checkpoint_path:
-      # Restores from checkpoint
-      saver.restore(sess, ckpt.model_checkpoint_path)
-      # Assuming model_checkpoint_path looks something like:
-      #   /my-favorite-path/cifar10_train/model.ckpt-0,
-      # extract global_step from it.
-      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-    else:
-      print('No checkpoint file found')
-      return
-
-    # Start the queue runners.
-    coord = tf.train.Coordinator()
-    try:
-      threads = []
-      for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
-        threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
-                                         start=True))
-
-      num_iter = int(math.ceil(NUM_EXAMPLES / FLAGS.batch_size))
-      true_count = 0  # Counts the number of correct predictions.
-      total_sample_count = num_iter * FLAGS.batch_size
-      step = 0
-      while step < num_iter and not coord.should_stop():
-        predictions = sess.run([loss_op])
-        true_count += np.sum(predictions)
-        step += 1
-
-      # Compute precision @ 1.
-      precision = true_count / total_sample_count
-      print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
-      sys.stdout.flush()
-
-      summary = tf.Summary()
-      summary.ParseFromString(sess.run(summary_op))
-      summary.value.add(tag='Precision @ 1', simple_value=precision)
-      summary_writer.add_summary(summary, global_step)
-    except Exception as e:  # pylint: disable=broad-except
-      coord.request_stop(e)
-
-    coord.request_stop()
-    coord.join(threads, stop_grace_period_secs=10)
-
-
 def evaluate():
-  """Eval CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default() as g:
-    # Get images and labels for CIFAR-10.
-    images, labels = steer.inputs()
+    with tf.Graph().as_default():
 
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    logits = steer.inference(images)
+        # Get images and labels
+        # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
+        # GPU and resulting in a slow down.
+        # with tf.device('/cpu:0'):
+        images, labels = steer.inputs()
 
-    # Calculate predictions.
-    loss_op = steer.loss(logits, labels)
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        logits = steer.inference(images)
+        loss_op = steer.loss(logits, labels)
 
-    saver = tf.train.Saver()
+        saver = tf.train.Saver()
+        print("Evaluating")
+        sys.stdout.flush()
+        with tf.Session() as sess:
+            ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+            if ckpt and ckpt.model_checkpoint_path:
+                # Restores from checkpoint
+                print("Restoring session "+ckpt.model_checkpoint_path)
+                sys.stdout.flush()
+                # Assuming model_checkpoint_path looks something like:
+                #   /my-favorite-path/cifar10_train/model.ckpt-0,
+                # extract global_step from it.
+                saver.restore(sess,ckpt.model_checkpoint_path)
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                print(global_step)
+                sys.stdout.flush()
+            else:
+                print('No checkpoint file found')
+                return
 
-    while True:
-      eval_once(saver, loss_op)
-      break
 
+        # Start the queue runners.
+        coord = tf.train.Coordinator()
+        try:
+            threads = []
+            for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+                threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+                                                                                 start=True))
+
+            num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+            true_count = 0    # Counts the number of correct predictions.
+            total_sample_count = num_iter * FLAGS.batch_size
+            step = 0
+            while not coord.should_stop():
+                predictions = sess.run([top_k_op])
+                true_count += np.sum(predictions)
+                step += 1
+
+            # Calculate loss.
+            print(str(datetime.now()) + " " + sess.run(loss_op))
+            sys.stdout.flush()
+
+        except Exception as e:    # pylint: disable=broad-except
+            coord.request_stop(e)
+
+        coord.request_stop()
+        coord.join(threads, stop_grace_period_secs=10)
 
 def main(argv=None):  # pylint: disable=unused-argument
-  if tf.gfile.Exists(FLAGS.eval_dir):
-    tf.gfile.DeleteRecursively(FLAGS.eval_dir)
-  tf.gfile.MakeDirs(FLAGS.eval_dir)
-  evaluate()
+    if tf.gfile.Exists(EVAL_DIR):
+        tf.gfile.DeleteRecursively(EVAL_DIR)
+    tf.gfile.MakeDirs(EVAL_DIR)
+    evaluate()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
--- steer_input.py
+++ steer_input.py
@@ -38,152 +38,145 @@ GLOBALWIDTH=640
 
 
 def get_names_and_labels():
-  labelfile=open(LOG_FILE,"r")
-  line=labelfile.readline()
-  files=[]
-  labels=[]
-  while(len(line)>0):
-    totalstring=line.split(" ")
-    files.append(DATA_DIR+totalstring[0])
-    labels.append(float(totalstring[1]))
+    labelfile=open(LOG_FILE,"r")
     line=labelfile.readline()
-  return files,labels
+    files=[]
+    labels=[]
+    while(len(line)>0):
+        totalstring=line.split(" ")
+        files.append(DATA_DIR+totalstring[0])
+        labels.append(float(totalstring[1]))
+        line=labelfile.readline()
+    return files,labels
 
 def read_image(filename_queue):
-  class ImageRecord(object):
-    pass
-  result=ImageRecord()
-  #tensorkey,image_file=image_reader.read(filename_queue)
+    class ImageRecord(object):
+        pass
+    result=ImageRecord()
+    #tensorkey,image_file=image_reader.read(filename_queue)
 
-  image_file=tf.read_file(filename_queue[0])
-  result.label=filename_queue[1]
-  result.uint8image=tf.image.decode_png(image_file)
-  return result
+    image_file=tf.read_file(filename_queue[0])
+    result.label=filename_queue[1]
+    result.uint8image=tf.image.decode_png(image_file)
+    return result
 
 
-def _generate_image_and_label_batch(image, label, min_queue_examples,
-                                    batch_size, shuffle):
-  """Construct a queued batch of images and labels.
-  Args:
-    image: 3-D Tensor of [height, width, 3] of type.float32.
-    label: 1-D Tensor of type.int32
-    min_queue_examples: int32, minimum number of samples to retain
-      in the queue that provides of batches of examples.
-    batch_size: Number of images per batch.
-    shuffle: boolean indicating whether to use a shuffling queue.
-  Returns:
-    images: Images. 4D tensor of [batch_size, height, width, 3] size.
-    labels: Labels. 1D tensor of [batch_size] size.
-  """
-  # Create a queue that shuffles the examples, and then
-  # read 'batch_size' images + labels from the example queue.
-  if shuffle:
-    images, label_batch = tf.train.shuffle_batch(
-        [image, label],
-        batch_size=batch_size,
-        num_threads=16,
-        capacity=min_queue_examples + 3 * batch_size,
-        min_after_dequeue=min_queue_examples)
-  else:
-    images, label_batch = tf.train.batch(
-        [image, label],
-        batch_size=batch_size,
-        num_threads=16,
-        capacity=min_queue_examples + 3 * batch_size)
+def _generate_image_and_label_batch(image, label, min_queue_examples,batch_size, shuffle):
+    """Construct a queued batch of images and labels.
+    Args:
+        image: 3-D Tensor of [height, width, 3] of type.float32.
+        label: 1-D Tensor of type.int32
+        min_queue_examples: int32, minimum number of samples to retain
+            in the queue that provides of batches of examples.
+        batch_size: Number of images per batch.
+        shuffle: boolean indicating whether to use a shuffling queue.
+    Returns:
+        images: Images. 4D tensor of [batch_size, height, width, 3] size.
+        labels: Labels. 1D tensor of [batch_size] size.
+    """
+    # Create a queue that shuffles the examples, and then
+    # read 'batch_size' images + labels from the example queue.
+    if shuffle:
+        images, label_batch = tf.train.shuffle_batch(
+            [image, label],
+            batch_size=batch_size,
+            num_threads=16,
+            capacity=min_queue_examples + 3 * batch_size,
+            min_after_dequeue=min_queue_examples)
+    else:
+        images, label_batch = tf.train.batch(
+            [image, label],
+            batch_size=batch_size,
+            num_threads=16,
+            capacity=min_queue_examples + 3 * batch_size)
 
-  return images, tf.reshape(label_batch, [batch_size])
+    return images, tf.reshape(label_batch, [batch_size])
 
 
 def distorted_inputs(data_dir, batch_size): #MUST SET HEIGHT AND WIDTH
-  """Construct distorted input for training using the Reader ops.
-  Args:
-    data_dir: Path to the data directory.
-    batch_size: Number of images per batch.
-  Returns:
-    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
-    labels: Labels. 1D tensor of [batch_size] size.
-  """
+    """Construct distorted input for training using the Reader ops.
+    Args:
+        data_dir: Path to the data directory.
+        batch_size: Number of images per batch.
+    Returns:
+        images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+        labels: Labels. 1D tensor of [batch_size] size.
+    """
 
-  filenamelist,labellist=get_names_and_labels()
-  images = tf.convert_to_tensor(filenamelist, dtype=tf.string)
-  labels = tf.convert_to_tensor(labellist, dtype=tf.float32) 
-  
-  input_queue = tf.train.slice_input_producer([images, labels])
-  read_input=read_image(input_queue)
+    filenamelist,labellist=get_names_and_labels()
+    images = tf.convert_to_tensor(filenamelist, dtype=tf.string)
+    labels = tf.convert_to_tensor(labellist, dtype=tf.float32) 
+    
+    input_queue = tf.train.slice_input_producer([images, labels])
+    read_input=read_image(input_queue)
 
-  reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+    reshaped_image = tf.cast(read_input.uint8image, tf.float32)
 
-  # Image processing for training the network. Note the many random
-  # distortions applied to the image.
+    # Image processing for training the network. Note the many random
+    # distortions applied to the image.
 
-  # Randomly flip the image horizontally.
-  #distorted_image = tf.image.random_flip_left_right(reshaped)
-  #Cannot do this so easily because we must change the label accordingly. Needs additional work
-  print("Distorting images")
-  sys.stdout.flush()
+    # Randomly flip the image horizontally.
+    #distorted_image = tf.image.random_flip_left_right(reshaped)
+    #Cannot do this so easily because we must change the label accordingly. Needs additional work
+    print("Distorting images")
+    sys.stdout.flush()
 
-  # Because these operations are not commutative, consider randomizing
-  # the order their operation.
-  # NOTE: since per_image_standardization zeros the mean and makes
-  # the stddev unit, this likely has no effect see tensorflow#1458.
-  distorted_image = tf.image.random_brightness(reshaped_image,
-                                               max_delta=63)
-  distorted_image = tf.image.random_contrast(distorted_image,
-                                             lower=0.2, upper=1.8)
+    # Because these operations are not commutative, consider randomizing
+    # the order their operation.
+    # NOTE: since per_image_standardization zeros the mean and makes
+    # the stddev unit, this likely has no effect see tensorflow#1458.
+    distorted_image = tf.image.random_brightness(reshaped_image,max_delta=63)
+    distorted_image = tf.image.random_contrast(distorted_image,lower=0.2, upper=1.8)
 
-  # Subtract off the mean and divide by the variance of the pixels.
-  float_image = tf.image.per_image_standardization(distorted_image)
+    # Subtract off the mean and divide by the variance of the pixels.
+    float_image = tf.image.per_image_standardization(distorted_image)
 
-  # Set the shapes of tensors.
-  float_image.set_shape([GLOBALHEIGHT, GLOBALWIDTH, 3])
-  read_input.label.set_shape([])
+    # Set the shapes of tensors.
+    float_image.set_shape([GLOBALHEIGHT, GLOBALWIDTH, 3])
+    read_input.label.set_shape([])
 
-  # Ensure that the random shuffling has good mixing properties.
-  min_fraction_of_examples_in_queue = 0.4
-  min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *min_fraction_of_examples_in_queue)
-  print ('Filling queue with %d images before starting to train. '
-         'This will take a few minutes.' % min_queue_examples)
-  sys.stdout.flush()
+    # Ensure that the random shuffling has good mixing properties.
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *min_fraction_of_examples_in_queue)
+    print ('Filling queue with %d images before starting to train. '
+             'This will take a few minutes.' % min_queue_examples)
+    sys.stdout.flush()
 
-  # Generate a batch of images and labels by building up a queue of examples.
-  return _generate_image_and_label_batch(float_image, read_input.label,
-                                         min_queue_examples, batch_size,
-                                         shuffle=True)
+    # Generate a batch of images and labels by building up a queue of examples.
+    return _generate_image_and_label_batch(float_image, read_input.label,min_queue_examples, batch_size,shuffle=True)
 
 def inputs(data_dir, batch_size):
-  """Construct distorted input for training using the Reader ops.
-  Args:
+    """Construct distorted input for training using the Reader ops.
+    Args:
     data_dir: Path to the data directory.
     batch_size: Number of images per batch.
-  Returns:
-    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
-    labels: Labels. 1D tensor of [batch_size] size.
-  """
+    Returns:
+        images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+        labels: Labels. 1D tensor of [batch_size] size.
+    """
 
-  filenamelist,labellist=get_names_and_labels()
-  images = tf.convert_to_tensor(filenamelist, dtype=tf.string)
-  labels = tf.convert_to_tensor(labellist, dtype=tf.float32) 
-  
-  input_queue = tf.train.slice_input_producer([images, labels])
-  read_input=read_image(input_queue)
+    filenamelist,labellist=get_names_and_labels()
+    images = tf.convert_to_tensor(filenamelist, dtype=tf.string)
+    labels = tf.convert_to_tensor(labellist, dtype=tf.float32) 
+    
+    input_queue = tf.train.slice_input_producer([images, labels])
+    read_input=read_image(input_queue)
 
-  reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+    reshaped_image = tf.cast(read_input.uint8image, tf.float32)
 
-  # Subtract off the mean and divide by the variance of the pixels.
-  float_image = tf.image.per_image_standardization(reshaped_image)
+    # Subtract off the mean and divide by the variance of the pixels.
+    float_image = tf.image.per_image_standardization(reshaped_image)
 
-  # Set the shapes of tensors.
-  float_image.set_shape([GLOBALHEIGHT, GLOBALWIDTH, 3])
-  read_input.label.set_shape([])
+    # Set the shapes of tensors.
+    float_image.set_shape([GLOBALHEIGHT, GLOBALWIDTH, 3])
+    read_input.label.set_shape([])
 
-  # Ensure that the random shuffling has good mixing properties.
-  min_fraction_of_examples_in_queue = 0.4
-  min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *min_fraction_of_examples_in_queue)
-  print ('Filling queue with %d images before starting to eval. '
-         'This will take a few minutes.' % min_queue_examples)
+    # Ensure that the random shuffling has good mixing properties.
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *min_fraction_of_examples_in_queue)
+    print ('Filling queue with %d images before starting to eval. '
+                 'This will take a few minutes.' % min_queue_examples)
+    sys.stdout.flush()
 
-  # Generate a batch of images and labels by building up a queue of examples.
-  return _generate_image_and_label_batch(float_image, read_input.label,
-                                         min_queue_examples, batch_size,
-                                         shuffle=True)
-
+    # Generate a batch of images and labels by building up a queue of examples.
+    return _generate_image_and_label_batch(float_image, read_input.label,min_queue_examples, batch_size,shuffle=True)
