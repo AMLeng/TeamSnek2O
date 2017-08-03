e72463bc2b612b8495c0107bbd40a20de1a7f441
edelmanjm <edelmanjm@users.noreply.github.com>
2017-08-03 13:27:09 -0700

    Add Windows wheel

--- controllers/windows_wheel.py
+++ controllers/windows_wheel.py
@@ -1,6 +1,30 @@
-class windows_wheel:
+import threading
+import pyvjoy
+from pyvjoy import vjoydevice
+
+class WindowsWheel(threading.Thread):
+    lock = threading.Lock()
+    running = True
+
+    joystick = vjoydevice.VJoyDevice(1)
+
     def __init__(self):
+        threading.Thread.__init__(self, daemon=True)
+        with WindowsWheel.lock:
+            WindowsWheel.running = True
+
+
+    def stop(self):
+        pass
+
+    # Don't think anything needs to happen here
+    def run(self):
         pass
 
     def set_angle(self, angle):
-        pass
+        if WindowsWheel.running:
+            # translating from -180-180 to 0x0 to 0x8000
+            step = int(angle / 180 * 16384 + 16384)
+            self.joystick.set_axis(pyvjoy.constants.HID_USAGE_X, step)
+            # For some reason running this line makes it so it doesn't update. Yup.
+            # self.joystick.update()
